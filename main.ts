b44aeb98994dbea5491faf90563c10b0317f0482
lhlyu <misakafs@gmail.com>
2025-10-30 20:17:31 +0800

    style: format codes

--- main.ts
+++ main.ts
@@ -13,24 +13,23 @@ try {
 	throw err;
 }
 
-
 // 启动服务
 Deno.serve(async (req) => {
-    const url = new URL(req.url);
+	const url = new URL(req.url);
 
 	if (url.pathname === '/api/config') {
-        const body = JSON.stringify({
-            code: 0,
-            msg: 'ok',
-            data: config
-        });
-        return new Response(body, {
-            status: 200,
-            headers: {
-                'content-type': 'application/json; charset=utf-8',
-            },
-        });
-    }
+		const body = JSON.stringify({
+			code: 0,
+			msg: 'ok',
+			data: config,
+		});
+		return new Response(body, {
+			status: 200,
+			headers: {
+				'content-type': 'application/json; charset=utf-8',
+			},
+		});
+	}
 
 	return new Response('一切正常', {
 		status: 200,
